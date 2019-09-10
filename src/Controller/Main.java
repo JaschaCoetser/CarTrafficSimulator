f0e19577ddfbb3c82c44d24f3b2d5e36456ca4a3
Jascha <jascha.coetser@my.jcu.edu.au>
2019-09-10 13:33:36 +1000

    Implemented Road Class

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,8 +1,11 @@
 package Controller;
 
+import View.World;
+
 public class Main {
     public static void main(String[] args) {
-
+        World world = new World();
+        System.out.println();
     }
 
     // Make a world class , Collection of roads
--- src/View/World.java
+++ src/View/World.java
@@ -1,6 +1,43 @@
 package View;
 
+// Imports go here
+
+import java.util.ArrayList;
+
+// This is the Title
 public class World {
-    // Array list for roads
+
+    // these are class variables
+    private ArrayList<Road> roads = new ArrayList<>();
+
+    // This is a default constructor
+    public World() {
+        for (int i = 0; i < 5; ++i) {
+            Road tempRoad = new Road();
+            roads.add(tempRoad);
+            // First
+            if (i == 0) {
+                continue;
+                // Last
+            } else if (i == 4) {
+                tempRoad.previous_road = roads.get(i - 1);
+            // Middle
+            } else {
+                tempRoad.previous_road = roads.get(i - 1);
+                roads.get(i - 1).next_road = tempRoad;
+//                tempRoad.next_road = roads.get(i + 1);
+            }
+        }
+    }
+
+    // This is a non-default constructor
+    // public World( ) {
+
+    // }
+
+    // methods
+
+
+    // getters and setters
 
 }
