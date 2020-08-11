45da2b96c3b6b1154f5bc7e03134b02817d7f3ee
JaschaCoetser <jascha.coetser@my.jcu.edu.au>
2020-08-11 19:50:47 +1000

    This is the truth

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         World world = new World(50);
         System.out.println();
+        System.out.println("This is the world");
     }
 }
